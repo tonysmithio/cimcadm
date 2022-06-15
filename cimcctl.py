fe7cf7af64375c947dcfdd64e8c9a972e8392870
Edwin Anthony Smith <tony@tonysmith.io>
2022-06-15 10:08:05 -0500

    add logic to read yaml file

--- cimcctl.py
+++ cimcctl.py
@@ -2,8 +2,51 @@
 
 import argparse
 import yaml
+from imcsdk.imchandle import ImcHandle
+
 
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--config', help='config file in yaml format')
 
-if __name__ == __main__:
+    args = parser.parse_args()
+    
+    with open(args.config, 'r') as configFile:
+        data = yaml.load(configFile, Loader=yaml.FullLoader)
+
+#    print(data)
+
+    hosts = []
+
+    cimc_ips= []
+
+    svr_count = len(data['svrs'])
+
+    i = 0
+
+    while i < svr_count:
+        
+        for a in data['svrs'][i]:
+            hosts.append(a)
+
+
+        for b in data['svrs'][i]:
+            cimc_ips.append(data['svrs'][i][b]['cimc_ip'])
+
+
+        i += 1
+
+    print(hosts)
+    print(cimc_ips)
+
+
+    
+
+'''
+   # print(hostnames)
+   # print(data['cimc_user'])
+   # print(data['cimc_passwd'])
+'''
+    
+if __name__ == "__main__":
     main()
