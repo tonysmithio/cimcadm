397189b86ba6876cbadf07a4b8de323db1fb7e6c
Edwin Anthony Smith <tony@tonysmith.io>
2022-07-12 12:59:54 -0500

    Added function for cleaning disks

--- cimcadm.py
+++ cimcadm.py
@@ -15,6 +15,7 @@ from imcsdk.imchandle import ImcHandle
 from imcsdk.apis.server.inventory import inventory_get
 from imcsdk.apis.server.health import faults_get
 from imcsdk.utils.imcfirmwareinstall import *
+from imcsdk.apis.server.storage import *
 
 
 log_colors_dict = {'INFO':'white','DEBUG':'cyan','WARNING':'bold_yellow','ERROR':'red','CRITICAL':'bold_red'}
@@ -36,6 +37,7 @@ parser.add_argument('--get-inv', metavar='', help='Collect and display inventory
 parser.add_argument('--get-faults', help='Get Faults of Chassis', action='store_true')
 parser.add_argument('--set-hostname', help='Update CIMC Hostname', action='store_true')
 parser.add_argument('--update-firmware', help='Initiate firmware update', action='store_true')
+parser.add_argument('--clean-disks', help='Clear boot-drive, remove all virtual disks, and reset all physical disks', action='store_true')
 
 args = parser.parse_args()
 
@@ -116,9 +118,9 @@ def firmwareUpdate(ip,user,password):
     try:
         handle = ImcHandle(ip,user,password)
         handle.login()
-        firmware_update(handle=handle, remote_share=data['huu_iso'], share_type=data['huu_protocol'], remote_ip=data['huu_svr_ip'],
-                username='', password='', update_component=data['huu_component'], stop_on_error='yes', timeout=90, verify_update='yes',
-                cimc_secure_boot='no', server_id=1, force=data['huu_force'], interval=20, backup_fw=False)
+        firmware_update(handle=handle, remote_share=data['huu']['iso'], share_type=data['huu']['protocol'], remote_ip=data['huu']['svr'],
+                username=data['huu']['user'], password=data['huu']['passwd'], update_component=data['huu']['component'], stop_on_error='yes', timeout=90, verify_update='yes',
+                cimc_secure_boot='no', server_id=1, force=data['huu']['force'], interval=20, backup_fw=False)
         handle.logout()
     except urllib.error.URLError as e1:
         eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
@@ -127,7 +129,51 @@ def firmwareUpdate(ip,user,password):
 
 
 
+def cleanDisks(ip,user,password):
+    try:
+        handle = ImcHandle(ip,user,password)
+        handle.login()
+        raid_controller = handle.query_dn('sys/rack-unit-1/board/storage-SAS-MRAID')
+        raid_controller.admin_action = 'clear-boot-drive'
+        handle.set_mo(raid_controller)
+        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Cleared Boot Drive')
+        raid_controller.admin_action = 'delete-all-vds-reset-pds'
+        handle.set_mo(raid_controller)
+        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Removed virtual disks & reset physical disks')
+        handle.logout()
+    except urllib.error.URLError as e1:
+        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+    except imcsdk.imcexception.ImcException as e2:
+        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+
+
 def main():
+
+
+    if args.clean_disks:
+        threads = []
+        for z in range(0,svr_count):
+            if 'cimc_user' in data['svrs'][z]:
+                user = data['svrs'][z]['cimc_user']
+            elif 'cimc_user' not in data['svrs'][z] and 'cimc_user' in data:
+                user = data['cimc_user']
+            elif 'cimc_user' not in data['svrs'][z] and 'cimc_user' not in data:
+                raise ValueError(data['svrs'][z]['name']+' is missing "cimc_user" in config file')
+
+            if 'cimc_passwd' in data['svrs'][z]:
+                passwd = data['svrs'][z]['cimc_passwd']
+            elif 'cimc_passwd' not in data['svrs'][z] and 'cimc_passwd' in data:
+                passwd = data['cimc_passwd']
+            elif 'cimc_passwd' not in data['svrs'][z] and 'cimc_passwd' not in data:
+                raise ValueError(data['svrs'][z]['name']+' is missing "cimc_passwd" in config file')
+
+        for z in range(0,svr_count):
+            thread = Thread(target=cleanDisks, args=(data['svrs'][z]['cimc_ip'],user,passwd))
+            threads.append(thread)
+            thread.start()
+        for thread in threads:
+            thread.join()
+
 
 
     if args.update_firmware:
