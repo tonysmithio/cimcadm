a45e02e5911d4e860884805c54f0eae1cd6da52c
Edwin Anthony Smith <69742294+tonysmithio@users.noreply.github.com>
2022-07-17 10:07:19 -0500

    Converted spaces to tabs

--- cimcadm.py
+++ cimcadm.py
@@ -48,138 +48,138 @@ args = parser.parse_args()
 
 
 with open(args.config, 'r') as configFile:
-        data = yaml.load(configFile, Loader=yaml.FullLoader)
+		data = yaml.load(configFile, Loader=yaml.FullLoader)
 
 
 svr_count = len(data['svrs'])
 
 
 def testLogin(ip,user,password):
-        try:
-            handle = ImcHandle(ip,user,password)
-            handle.login()
-            eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Login Successful.')
-            handle.logout()
-        except urllib.error.URLError as e1:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-        except imcsdk.imcexception.ImcException as e2:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+		try:
+			handle = ImcHandle(ip,user,password)
+			handle.login()
+			eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Login Successful.')
+			handle.logout()
+		except urllib.error.URLError as e1:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+		except imcsdk.imcexception.ImcException as e2:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
 
 def getFaults(ip,user,password): 
-        try:
-            handle = ImcHandle(ip,user,password)
-            handle.login()
-            faults = faults_get(handle=handle)
-            if len(faults) == 0:
-                eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | No Faults!')
-            elif len(faults) > 0:
-               for f in faults:
-                   faults_dict = {}
-                   for key,value in f.__dict__.items():
-                      faults_dict.update({key: value})
-                   eventLogger.critical('cimc-ip: '+handle._ImcSession__ip+' | '+faults_dict['descr'])
-            handle.logout()
-        except urllib.error.URLError as e1:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-        except imcsdk.imcexception.ImcException as e2:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+		try:
+			handle = ImcHandle(ip,user,password)
+			handle.login()
+			faults = faults_get(handle=handle)
+			if len(faults) == 0:
+				eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | No Faults!')
+			elif len(faults) > 0:
+			   for f in faults:
+				   faults_dict = {}
+				   for key,value in f.__dict__.items():
+					  faults_dict.update({key: value})
+				   eventLogger.critical('cimc-ip: '+handle._ImcSession__ip+' | '+faults_dict['descr'])
+			handle.logout()
+		except urllib.error.URLError as e1:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+		except imcsdk.imcexception.ImcException as e2:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
 
 def getInventory(ip,user,password):
-        try:
-           handle = ImcHandle(ip,user,password)
-           handle.login()
-           inv = inventory_get(handle=handle, component=args.get_inv)
-           inv_yaml = yaml.dump(inv, sort_keys=False)
-           handle.logout()
-           eventLogger.info(inv_yaml)
-        except urllib.error.URLError as e1:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-        except imcsdk.imcexception.ImcException as e2:
-            eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+		try:
+		   handle = ImcHandle(ip,user,password)
+		   handle.login()
+		   inv = inventory_get(handle=handle, component=args.get_inv)
+		   inv_yaml = yaml.dump(inv, sort_keys=False)
+		   handle.logout()
+		   eventLogger.info(inv_yaml)
+		except urllib.error.URLError as e1:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+		except imcsdk.imcexception.ImcException as e2:
+			eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
-        
+		
 def setHostname(ip,user,password,name):
-    try:
-        handle = ImcHandle(ip,user,password)
-        handle.login()
-        mgmtif = handle.query_dn('sys/rack-unit-1/mgmt/if-1')
-        if mgmtif.hostname == name:
-            handle.logout()
-            eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | cimc-hostname is already set to "'+mgmtif.hostname+'"')
-        elif mgmtif.hostname != name:
-            mgmtif.hostname = name
-            handle.set_mo(mgmtif)
-            handle.logout()
-            eventLogger.warning('cimc-ip: '+handle._ImcSession__ip+' | cimc-hostname has been changed to "'+name+'"')
-    except urllib.error.URLError as e1:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-    except imcsdk.imcexception.ImcException as e2:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+	try:
+		handle = ImcHandle(ip,user,password)
+		handle.login()
+		mgmtif = handle.query_dn('sys/rack-unit-1/mgmt/if-1')
+		if mgmtif.hostname == name:
+			handle.logout()
+			eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | cimc-hostname is already set to "'+mgmtif.hostname+'"')
+		elif mgmtif.hostname != name:
+			mgmtif.hostname = name
+			handle.set_mo(mgmtif)
+			handle.logout()
+			eventLogger.warning('cimc-ip: '+handle._ImcSession__ip+' | cimc-hostname has been changed to "'+name+'"')
+	except urllib.error.URLError as e1:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+	except imcsdk.imcexception.ImcException as e2:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
 
 def firmwareUpdate(ip,user,password):
-    try:
-        handle = ImcHandle(ip,user,password)
-        handle.login()
-        firmware_update(handle=handle, remote_share=data['huu']['iso'], share_type=data['huu']['protocol'], remote_ip=data['huu']['svr'],
-                username=data['huu']['user'], password=data['huu']['passwd'], update_component=data['huu']['component'], stop_on_error='yes', timeout=90, verify_update='yes',
-                cimc_secure_boot='no', server_id=1, force=data['huu']['force'], interval=20, backup_fw=False)
-        handle.logout()
-    except urllib.error.URLError as e1:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-    except imcsdk.imcexception.ImcException as e2:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+	try:
+		handle = ImcHandle(ip,user,password)
+		handle.login()
+		firmware_update(handle=handle, remote_share=data['huu']['iso'], share_type=data['huu']['protocol'], remote_ip=data['huu']['svr'],
+				username=data['huu']['user'], password=data['huu']['passwd'], update_component=data['huu']['component'], stop_on_error='yes', timeout=90, verify_update='yes',
+				cimc_secure_boot='no', server_id=1, force=data['huu']['force'], interval=20, backup_fw=False)
+		handle.logout()
+	except urllib.error.URLError as e1:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+	except imcsdk.imcexception.ImcException as e2:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
 
 
 def cleanDisks(ip,user,password):
-    try:
-        handle = ImcHandle(ip,user,password)
-        handle.login()
-        raid_controller = handle.query_dn('sys/rack-unit-1/board/storage-'+data['storage']['ctlr_type']+'-'+data['storage']['ctlr_slot'])
-        raid_controller.admin_action = 'clear-boot-drive'
-        handle.set_mo(raid_controller)
-        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Cleared Boot Drive')
-        raid_controller.admin_action = 'delete-all-vds-reset-pds'
-        handle.set_mo(raid_controller)
-        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Removed virtual disks & reset physical disks')
-        handle.logout()
-    except urllib.error.URLError as e1:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-    except imcsdk.imcexception.ImcException as e2:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
+	try:
+		handle = ImcHandle(ip,user,password)
+		handle.login()
+		raid_controller = handle.query_dn('sys/rack-unit-1/board/storage-'+data['storage']['ctlr_type']+'-'+data['storage']['ctlr_slot'])
+		raid_controller.admin_action = 'clear-boot-drive'
+		handle.set_mo(raid_controller)
+		eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Cleared Boot Drive')
+		raid_controller.admin_action = 'delete-all-vds-reset-pds'
+		handle.set_mo(raid_controller)
+		eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Removed virtual disks & reset physical disks')
+		handle.logout()
+	except urllib.error.URLError as e1:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+	except imcsdk.imcexception.ImcException as e2:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')
 
 
 
 
 def createBootDisk(ip,user,password):
-    try:
-        handle = ImcHandle(ip,user,password)
-        drive_list = []
-        drive_list.append(data['storage']['bootdisk']['pd_grp'])
-        cleanDisks(ip,user,password)
-        handle.login()
-        virtual_drive_create(handle=handle,virtual_drive_name=data['storage']['bootdisk']['vd_name'],
-                raid_level=data['storage']['bootdisk']['raid_lvl'],
-                drive_group=drive_list,
-                size=data['storage']['bootdisk']['size'],
-                controller_type=data['storage']['ctlr_type'],
-                controller_slot=data['storage']['ctlr_slot'])
-        handle.logout()
-        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Created "'+data['storage']['bootdisk']['vd_name']+'" virtual disk.')
-        handle.login()
-        virtual_drive_set_boot_drive(handle=handle,
-                controller_type=data['storage']['ctlr_type'],
-                controller_slot=data['storage']['ctlr_slot'],
-                name=data['storage']['bootdisk']['vd_name'])
-        handle.logout()
-        eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Set "Boot Drive" parameter to "true".')
-    except urllib.error.URLError as e1:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
-    except imcsdk.imcexception.ImcException as e2:
-        eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')    
+	try:
+		handle = ImcHandle(ip,user,password)
+		drive_list = []
+		drive_list.append(data['storage']['bootdisk']['pd_grp'])
+		cleanDisks(ip,user,password)
+		handle.login()
+		virtual_drive_create(handle=handle,virtual_drive_name=data['storage']['bootdisk']['vd_name'],
+				raid_level=data['storage']['bootdisk']['raid_lvl'],
+				drive_group=drive_list,
+				size=data['storage']['bootdisk']['size'],
+				controller_type=data['storage']['ctlr_type'],
+				controller_slot=data['storage']['ctlr_slot'])
+		handle.logout()
+		eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Created "'+data['storage']['bootdisk']['vd_name']+'" virtual disk.')
+		handle.login()
+		virtual_drive_set_boot_drive(handle=handle,
+				controller_type=data['storage']['ctlr_type'],
+				controller_slot=data['storage']['ctlr_slot'],
+				name=data['storage']['bootdisk']['vd_name'])
+		handle.logout()
+		eventLogger.info('cimc-ip: '+handle._ImcSession__ip+' | Set "Boot Drive" parameter to "true".')
+	except urllib.error.URLError as e1:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Connection Failure.')
+	except imcsdk.imcexception.ImcException as e2:
+		eventLogger.error('cimc-ip: '+handle._ImcSession__ip+' | Authentication Failure.')    
 
 
 def main():
@@ -213,49 +213,49 @@ def main():
 		elif 'cimc_passwd' not in data['svrs'][z] and 'cimc_passwd' not in data:
 			raise ValueError(data['svrs'][z]['name']+' is missing "cimc_passwd" in config file')
 
-    if args.make_bootdisk:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(createBootDisk, svr_ips[s],svr_users[s],svr_passwds[s])
+	if args.make_bootdisk:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(createBootDisk, svr_ips[s],svr_users[s],svr_passwds[s])
 
 
-    if args.clean_disks:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):  
-                ex.submit(cleanDisks, svr_ips[s],svr_users[s],svr_passwds[s])
+	if args.clean_disks:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):  
+				ex.submit(cleanDisks, svr_ips[s],svr_users[s],svr_passwds[s])
 
 
-    if args.update_firmware:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(firmwareUpdate, svr_ips[s],svr_users[s],svr_passwds[s])
+	if args.update_firmware:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(firmwareUpdate, svr_ips[s],svr_users[s],svr_passwds[s])
 
 
-    if args.set_hostname:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(setHostname, svr_ips[s],svr_users[s],svr_passwds[s],svr_names[s])
+	if args.set_hostname:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(setHostname, svr_ips[s],svr_users[s],svr_passwds[s],svr_names[s])
 
 
-    if args.test_login:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(testLogin, svr_ips[s],svr_users[s],svr_passwds[s])
+	if args.test_login:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(testLogin, svr_ips[s],svr_users[s],svr_passwds[s])
 
 
-    if args.get_inv:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(getInventory, svr_ips[s],svr_users[s],svr_passwds[s])
+	if args.get_inv:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(getInventory, svr_ips[s],svr_users[s],svr_passwds[s])
 
 
-    if args.get_faults:
-        with ThreadPoolExecutor(args.threads) as ex:
-            for s in range(svr_count):
-                ex.submit(getFaults, svr_ips[s],svr_users[s],svr_passwds[s]) 
+	if args.get_faults:
+		with ThreadPoolExecutor(args.threads) as ex:
+			for s in range(svr_count):
+				ex.submit(getFaults, svr_ips[s],svr_users[s],svr_passwds[s]) 
 
 
 
 
 if __name__ == "__main__":
-    main()
+	main()
